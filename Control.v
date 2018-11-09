bdb1c30983c38fa50444f517996045649d633111
diCharles <43318661+diCharles@users.noreply.github.com>
2018-11-09 08:58:39 -0600

    Update Control.v

--- Control.v
+++ Control.v
@@ -39,7 +39,8 @@ always@(OP) begin
 		R_Type:       ControlValues= 11'b1_001_00_00_111;
 		I_Type_ADDI:  ControlValues= 11'b0_101_00_00_100;
 		I_Type_ORI:   ControlValues= 11'b0_101_00_00_101;
-		
+		I_Type_MOV:   ControlValues= 11'b0_101_00_00_101;
+		I_Type_DIV:   ControlValues= 11'b0_101_00_00_101;
 		default:
 			ControlValues= 10'b0000000000;
 		endcase
